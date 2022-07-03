84024fa56fbb13a368386302357f8e896e45959c
paintdaworld <108595940+paintdaworld@users.noreply.github.com>
2022-07-02 19:10:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,9 @@
-import os
+password_length = 8
+input_password = 9
 
-import openai
-from flask import Flask, redirect, render_template, request, url_for
-
-app = Flask(__name__)
-openai.api_key = os.getenv("OPENAI_API_KEY")
-
-
-@app.route("/", methods=("GET", "POST"))
-def index():
-    if request.method == "POST":
-        animal = request.form["animal"]
-        response = openai.Completion.create(
-            model="text-davinci-002",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
-        )
-        return redirect(url_for("index", result=response.choices[0].text))
-
-    result = request.args.get("result")
-    return render_template("index.html", result=result)
+if input_password < password_length:
+    print("not enough characters.")
+else:
+    print("perfect password length!")
 
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
